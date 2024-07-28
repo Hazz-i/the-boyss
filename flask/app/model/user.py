e6f9e3ceeb9edd46d84efad1c424164b1bad9825
Hazz-i <wahidh697@gmail.com>
2024-07-29 06:25:34 +0700

    add: connection to mysql

---
+++ flask/app/model/user.py
@@ -0,0 +1,13 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
