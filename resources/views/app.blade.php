f8cb4f522a4aae3d5188e27260f2203e7076178e
Hazz-i <wahidh697@gmail.com>
2024-09-19 05:25:47 +0700

    chore: fix  assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
+<html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" href="{{ asset('icon.svg') }}" type="svg">
